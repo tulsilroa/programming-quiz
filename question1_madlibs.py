f57df54e3f18d62dd34340f322f89fb2fde384f7
tulsilroa <tulsilroa@gmail.com>
2025-11-29 15:53:03 -0800

    Refactor MadLibs project for clarity and correctness
    Fixed formatting issues, corrected input prompts, and improved readability of the MadLibs project.

--- question1_madlibs.py
+++ question1_madlibs.py
@@ -1,36 +1,42 @@
-#This is a MadLibs project
-#My name: Tulsi
-#Who I collaborated with: n/a
+# This is a MadLibs project
+# My name: Tulsi
+# Who I collaborated with:na
+
 print("************************************")
-print("| |")
-print("| Welcome to MadLibs | ")
-print("| |")
-print(************************************)
-(''MadLibs is a fill-in-the-blanks story
+print("|                                  |")
+print("|        Welcome to MadLibs        |")
+print("|                                  |")
+print("************************************")
+
+print("""MadLibs is a fill-in-the-blanks story
 game. The player must choose words based on the
 given prompts, and the computer will return a
-short story that includes the words the user chose.''')
-play = onput("Do you want to play MadLibs?(y/n) ")
-if play = "y"
-person_name = input("Choose a name for a person: ")
-place = input("Choose a place: ")
-noun_1 = input("Choose a singlular noun: ")
-animal_1 = input(Choose an animal: )
-adjective_1 = input("Choose an adjective for a feeling: ")
-adjective_2 = input("Choose an adjective: ")
-adjective_3 input("Choose an adjective: ")
-animal_2 = input("Choose an animal: ")
-food = input("Choose a food: ")
-print("""
-Over break I am going camping with """ +person_name+""". It is important
-to be prepared when camping at place , so I made sure to pack a
-sleeping bag, flashlight, and a """ +noun_1+""". The possibility of seeing a
-""" +animal_1+""" makes me feel """ +adjective_1+""". I am excited to go
-hiking on the """ +adjective_2+""" trail. If I see a """ +adjective_3 +""" """
-+animal_2+"""
+short story that includes the words the user chose.""")
+
+play = input("Do you want to play MadLibs? (y/n) ")
+
+if play == "y":
+    person_name = input("Choose a name for a person: ")
+    place = input("Choose a place: ")
+    noun_1 = input("Choose a singular noun: ")
+    animal_1 = input("Choose an animal: ")
+    adjective_1 = input("Choose an adjective for a feeling: ")
+    adjective_2 = input("Choose an adjective: ")
+    adjective_3 = input("Choose an adjective: ")
+    animal_2 = input("Choose another animal: ")
+    food = input("Choose a food: ")
+
+    print("""
+Over break I am going camping with """ + person_name + """. It is important
+to be prepared when camping at """ + place + """, so I made sure to pack a
+sleeping bag, flashlight, and a """ + noun_1 + """. The possibility of seeing a
+""" + animal_1 + """ makes me feel """ + adjective_1 + """. I am excited to go
+hiking on the """ + adjective_2 + """ trail. If I see a """ + adjective_3 + """ """ + animal_2 + """
 on the hike, I will take it home as my new pet! The best part of
-camping is eating """ +food+""" by the campfire!
+camping is eating """ + food + """ by the campfire!
 """)
-print("Thanks for playing! Goodbye!")
+
+    print("Thanks for playing! Goodbye!")
+
 else:
-print("Goodbye!")
+    print("Goodbye!")
